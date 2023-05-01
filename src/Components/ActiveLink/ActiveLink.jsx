eed7694682f908eb2b0290d65fdef69e9d57cc83
dev-nirob1 <dev-nirob1@gmail.com>
2023-05-01 17:28:27 +0600

    active link added

---
+++ src/Components/ActiveLink/ActiveLink.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const ActiveLink = ({ to, children }) => {
+    return (
+        <NavLink
+            to={to}
+            className={({isActive})=>
+            `mr-10 text-lg font-medium text-[#757575] ${isActive ? 'text-[#7E90FE]' : ''}`}
+        >
+            {children}
+        </NavLink>
+    );
+};
+
+export default ActiveLink;
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
 import { Link } from 'react-router-dom';
+import ActiveLink from '../ActiveLink/ActiveLink';
 
 const Header = () => {
     return (
         <div className='flex border justify-between items-center container mx-auto h-20'>
             <h2 className='text-4xl font-bold'>JobFinder</h2>
-            <nav className=''>
-                <Link className='mr-10' to="/">Home</Link>
-                <Link className='mr-10' to="/statistics">Statistics</Link>
-                <Link className='mr-10' to="/">Applied Jobs</Link>
-                <Link className='mr-10' to="/">Blog</Link>
+            <nav>
+                <ActiveLink to="/">Home</ActiveLink>
+                <ActiveLink to="/statistics">Statistics</ActiveLink>
+                <ActiveLink to="/applied-job">Applied Jobs</ActiveLink>
+                <ActiveLink to="/blog">Blog</ActiveLink>
             </nav>
             <button className='text-[20px] text-white px-7 py-5 bg-gradient-to-r from-[#7E90FE] to-[#9873FF] rounded-lg'>Start Applying</button>
 
